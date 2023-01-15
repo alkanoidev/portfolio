c469185007a232e6c3b1f257270140ca033fc897
Filip Ivanovic <alkanoidev@gmail.com>
2023-01-15 01:20:40 +0100

    navbar

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				dark: "#000",
+				off: {
+					white: "#dcdcdc",
+					dark: "#1a1a1a",
+				},
+				primary: "#68af73",
+				secondary: "#62aeef",
+			}
+		},
+	},
+	plugins: [],
+}
