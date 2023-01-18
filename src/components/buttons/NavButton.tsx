d07e8e054f736680ec20cb705380f912d7e21f26
Filip Ivanovic <alkanoidev@gmail.com>
2023-01-18 17:17:54 +0100

    refactor

--- src/components/buttons/NavButton.tsx
+++ src/components/buttons/NavButton.tsx
@@ -2,12 +2,14 @@ import type { ReactNode } from "react";
 
 export default function NavButton({
   children,
+  href
 }: {
   children: string | ReactNode | ReactNode[];
+  href?: string;
 }) {
   return (
-    <button className="py-3 px-6 transition rounded-lg  hover:bg-off-dark hover:ring-2 hover:ring-off-dark hover:ring-offset-2 hover:ring-offset-transparent">
+    <a href={href} className="py-3 px-6 transition rounded-lg hover:bg-off-dark hover:ring-2 hover:ring-off-dark hover:ring-offset-2 hover:ring-offset-transparent hover:cursor-pointer">
       {children}
-    </button>
+    </a>
   );
 }
---
+++ src/sections/ProjectsSection.tsx
@@ -0,0 +1,86 @@
+import Project from "components/Project";
+import gsap from "gsap";
+import { useEffect } from "react";
+
+export default function ProjectsSection() {
+  useEffect(() => {
+    const timeline = gsap.timeline({ delay: 0.5 });
+    timeline.fromTo(
+      ".project",
+      {
+        opacity: 0,
+        x: -80,
+        ease: "power1.inOut",
+        scrollTrigger: {
+          trigger: ".projects",
+          scrub: 1,
+          start: "-20% center",
+          end: "80% center",
+        },
+        stagger: {
+          amount: 1,
+        },
+      },
+      {
+        opacity: 1,
+        x: 0,
+        ease: "power1.inOut",
+        scrollTrigger: {
+          trigger: ".projects",
+          scrub: 1,
+          start: "-20% center",
+          end: "80% center",
+        },
+        stagger: {
+          amount: 1,
+        },
+      }
+    );
+    timeline.fromTo(
+      ".thumbnail",
+      {
+        opacity: 0,
+        x: -50,
+        ease: "power1.inOut",
+        scrollTrigger: {
+          trigger: ".projects",
+          scrub: 1,
+          start: "-20% center",
+          end: "80% center",
+        },
+        stagger: {
+          amount: 1,
+        },
+      },
+      {
+        opacity: 1,
+        x: 0,
+        ease: "power1.inOut",
+        scrollTrigger: {
+          trigger: ".projects",
+          scrub: 1,
+          start: "-20% center",
+          end: "80% center",
+        },
+        stagger: {
+          amount: 1,
+        },
+      }
+    );
+
+    return () => {
+      timeline.clear();
+    };
+  }, []);
+
+  return (
+    <div id="projects-section" className="mt-52 h-full flex flex-col justify-center items-center gap-10">
+      <h1 className="text-5xl font-bold z-0">Projects</h1>
+      <div className="projects w-full flex flex-wrap justify-center">
+        <Project background="bg-primary/30" />
+        <Project background="bg-secondary/30" />
+        <Project background="bg-violet/30" />
+      </div>
+    </div>
+  );
+}
