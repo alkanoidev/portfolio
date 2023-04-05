e4f4ab499c784e9489d31d832bf6e79b9fa0b932
Filip Ivanovic <alkanoidev@gmail.com>
2023-04-06 00:28:53 +0200

    returned to prev

--- src/components/buttons/NavButton.tsx
+++ src/components/buttons/NavButton.tsx
@@ -11,7 +11,7 @@ export default function NavButton({
     <a
       href={href}
       aria-label={href}
-      className="py-3 px-6 transition rounded-lg hover:bg-zinc-700 hover:ring-2 hover:ring-zinc-700 hover:ring-offset-2 hover:ring-offset-transparent hover:cursor-pointer"
+      className="py-3 px-6 transition rounded-lg hover:bg-off-dark hover:ring-2 hover:ring-off-dark hover:ring-offset-2 hover:ring-offset-transparent hover:cursor-pointer"
     >
       {children}
     </a>
