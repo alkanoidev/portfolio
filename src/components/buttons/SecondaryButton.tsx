9143c8e1ca472a753e463d6b925fb1cec235cbca
Filip Ivanovic <alkanoidev@gmail.com>
2023-04-05 14:13:24 +0200

    secondary btn

--- src/components/buttons/SecondaryButton.tsx
+++ src/components/buttons/SecondaryButton.tsx
@@ -15,8 +15,8 @@ export default function SecondaryButton({
         className={classNames(
           "hover:bg-zinc-300 ring-2 ring-zinc-300 group rounded-2xl",
           "p-3 transition text-center font-semibold hover:text-dark",
-          "focus:bg-zinc-300 focus:text-dark focus:ring-2 focus:ring-zinc-300 focus:ring-offset-dark focus:ring-offset-4",
-          "active:bg-zinc-300 active:text-dark active:ring-2 active:ring-zinc-300 active:ring-offset-dark active:ring-offset-4"
+          "focus:bg-zinc-300 focus:text-dark",
+          "active:bg-zinc-300 active:text-dark"
         )}
       >
         {children}
@@ -27,8 +27,8 @@ export default function SecondaryButton({
       className={classNames(
         "hover:bg-zinc-300 ring-2 ring-zinc-300 group rounded-2xl",
         "p-3 transition text-center font-semibold hover:text-dark",
-        "focus:bg-zinc-300 focus:text-dark focus:ring-2 focus:ring-zinc-300 focus:ring-offset-dark focus:ring-offset-4",
-        "active:bg-zinc-300 active:text-dark active:ring-2 active:ring-zinc-300 active:ring-offset-dark active:ring-offset-4"
+        "focus:bg-zinc-300 focus:text-dark",
+        "active:bg-zinc-300 active:text-dark"
       )}
     >
       {children}
