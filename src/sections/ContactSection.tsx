7887c768ee6113baad8deef56a02acfd6903cde7
Filip Ivanovic <alkanoidev@gmail.com>
2023-05-28 23:11:28 +0200

    fix contact section responsive

--- src/sections/ContactSection.tsx
+++ src/sections/ContactSection.tsx
@@ -1,5 +1,5 @@
 import IconButton from "components/buttons/IconButton";
-import SecondaryButton from "components/buttons/OutlinedButton";
+import OutlinedButton from "components/buttons/OutlinedButton";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { FormEvent, useEffect, useRef, useState } from "react";
@@ -147,10 +147,29 @@ export default function ContactSection() {
                 </svg>
               }
             />
-            <SecondaryButton
-              href="mailto:alkanoidev@gmail.com"
-              title="alkanoidev@gmail.com"
-            />
+            <div className="hidden sm:block">
+              <OutlinedButton
+                href="mailto:alkanoidev@gmail.com"
+                title="alkanoidev@gmail.com"
+              />
+            </div>
+            <div className="sm:hidden block">
+              <IconButton
+                href="mailto:alkanoidev@gmail.com"
+                title="alkanoidev@gmail.com"
+                icon={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6 fill-primary-light dark:fill-primary-dark"
+                  >
+                    <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
+                    <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
+                  </svg>
+                }
+              />
+            </div>
           </div>
         </div>
         <form name="contact" method="POST" data-netlify="true" hidden>
